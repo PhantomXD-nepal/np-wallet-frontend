6e55d15d0d86b842a75a758f91c22e8c6201800c
PhantomDev <phantomarcanexd@gmail.com>
2025-07-05 11:39:20 +0545

    Fixed backend

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { useTransactions } from "../hooks/useTransactions";
+import { useEffect } from "react";
 
-export default function Index() {
+export default function Page() {
+  const { user } = useUser();
+  const { transactions, summary, isLoading, loadData, deleteTransaction } =
+    useTransactions(user?.id);
+
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  console.log(transactions);
+  console.log(summary);
+  console.log(user?.id);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.sdsdhshtsxs to edit this screen.</Text>
+    <View>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/signIn">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/signUp">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
     </View>
   );
 }
